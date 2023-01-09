c95a731e8ce4684836bf3d00ca6645aabeb85811
David Francisco <davidfco.pozo@gmail.com>
2023-01-09 09:12:48 -0400

    Profifle removedd
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- app.js
+++ app.js
@@ -34,6 +34,10 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+app.get("/", (req, res) => {
+  res.send("Jobs");
+});
+
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
